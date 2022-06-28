31c80634932d0829a4da498cc00920eb7f3918e4
ffuturesenior <shamray.ruslan068@gmail.com>
2022-06-28 17:13:33 +0300

    jopa

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar=({avatar,height,width})=>{
     return(
         <div>
             {avatar!=" "?
-                <div style={{position:"relative",border:"1px solid black",borderRadius:"45px",width:`${width}px`,height:`${height}px`}}><img style={{position:"absolute",height:"100%",width:"100%",objectFit:"cover"}} src={`https://abobasocial-server-dbsync.herokuapp.com/${avatar}`}/></div>
+                <div style={{position:"relative",overflow:"hidden",border:"1px solid black",borderRadius:"45px",width:`${width}px`,height:`${height}px`}}><img style={{position:"absolute",height:"100%",width:"100%",objectFit:"cover"}} src={`https://abobasocial-server-dbsync.herokuapp.com/${avatar}`}/></div>
             :
                 <div style={{border:"1px solid black",borderRadius:"45px",overflow:"hidden",width:`${width}px`,height:`${height}px`,background:"black"}}></div>
             }
